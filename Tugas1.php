2e54372f3c7af932cd111fb16c89cf3d727fa252
Albert <albert.bagil@gmail.com>
2021-11-09 17:41:17 +0700

    Tugas 1 PHP
    Tugas 1 PHP untuk dikumpulkan ke Niomic

---
+++ Tugas1.php
@@ -0,0 +1,24 @@
+<?php
+$ayamg = 13000;
+$ayamb = 15000;
+$ayams = 13000;
+$tempe = 1000;
+$tahu = 1000;
+$soto = 3000;
+$nasi = 5000;
+$teh = 4000;
+$tebu = 5000;
+
+echo "<h1>DAFTAR MENU</h1>";
+echo "<ul>
+  <li>Ayam Goreng &nbsp = Rp. $ayamg</li>
+  <li>Ayam Bakar &nbsp &nbsp = Rp. $ayamb</li>
+  <li>Ayam Sayur &nbsp &nbsp = Rp. $ayams</li>
+  <li>Tempe Goreng = Rp. $tempe</li>
+  <li>Tahu Goreng &nbsp &nbsp= Rp. $tahu</li>
+  <li>Soto &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp= Rp. $soto</li>
+  <li>Nasi Putih &nbsp &nbsp &nbsp &nbsp= Rp. $nasi</li>
+  <li>Es Teh &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp= Rp. $teh</li>
+  <li>Es Tebu &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp= Rp. $tebu</li>
+</ul>"
+ ?>
---
+++ Tugas1_2.php
@@ -0,0 +1,42 @@
+<?php
+$cireng = 1000;
+$duren = 15000;
+$milo = 20000;
+$teh = 5000;
+$piscok = 1000;
+
+echo "<h1>DAFTAR MENU</h1>";
+echo
+"<table border=1>
+  <tr>
+    <th>No</th>
+    <th>Nama Menu</th>
+    <th>Harga</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Cireng</td>
+    <td>Rp. $cireng</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Es Duren</td>
+    <td>Rp. $duren</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Es Milo</td>
+    <td>Rp. $milo</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>Es Teh</td>
+    <td>Rp. $teh</td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td>Piscok</td>
+    <td>Rp. $piscok</td>
+  </tr>
+</table>"
+ ?>
